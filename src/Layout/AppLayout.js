e3952ff8de883696e0de52ae46962a608d87ad7d
kirnjiyun <kimjiyunee@naver.com>
2024-05-24 21:16:25 +0900

    handleChange

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -9,35 +9,35 @@ import { userActions } from "../action/userAction";
 import { commonUiActions } from "../action/commonUiAction";
 
 const AppLayout = ({ children }) => {
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const user = { level: "admin" }; // 로그인 기능 만들고 지우기
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const user = null; // 로그인 기능 만들고 지우기
 
-  // const { user } = useSelector((state) => state.user);
-  useEffect(() => {
-    dispatch(userActions.loginWithToken());
-  }, []);
+    // const { user } = useSelector((state) => state.user);
+    useEffect(() => {
+        dispatch(userActions.loginWithToken());
+    }, []);
 
-  return (
-    <div>
-      <ToastMessage />
-      {location.pathname.includes("admin") ? (
-        <Row className="vh-100">
-          <Col xs={12} md={3} className="sidebar mobile-sidebar">
-            <Sidebar />
-          </Col>
-          <Col xs={12} md={9}>
-            {children}
-          </Col>
-        </Row>
-      ) : (
-        <>
-          <Navbar user={user} />
-          {children}
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <ToastMessage />
+            {location.pathname.includes("admin") ? (
+                <Row className="vh-100">
+                    <Col xs={12} md={3} className="sidebar mobile-sidebar">
+                        <Sidebar />
+                    </Col>
+                    <Col xs={12} md={9}>
+                        {children}
+                    </Col>
+                </Row>
+            ) : (
+                <>
+                    <Navbar user={user} />
+                    {children}
+                </>
+            )}
+        </div>
+    );
 };
 
 export default AppLayout;
