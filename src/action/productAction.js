3aba4486292178380e5188e6350a3997b0c46091
kirnjiyun <kimjiyunee@naver.com>
2024-06-03 19:56:32 +0900

    상품 추가기능 #3

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -6,15 +6,26 @@ import { commonUiActions } from "./commonUiAction";
 const getProductList = (query) => async (dispatch) => {};
 const getProductDetail = (id) => async (dispatch) => {};
 
-const createProduct = (formData) => async (dispatch) => {};
+const createProduct = (formData) => async (dispatch) => {
+    try {
+        dispatch({ type: types.PRODUCT_CREATE_REQUEST });
+        const response = await api.post("/product", formData);
+        if (response.status !== 200) throw new Error(response.error);
+        dispatch({ type: types.PRODUCT_CREATE_SUCCESS });
+        dispatch(commonUiActions.showToastMessage("상품생성완료", "success"));
+    } catch (error) {
+        dispatch({ type: types.PRODUCT_CREATE_FAIL, payload: error.error });
+        dispatch(commonUiActions.showToastMessage(error.error, "error"));
+    }
+};
 const deleteProduct = (id) => async (dispatch) => {};
 
 const editProduct = (formData, id) => async (dispatch) => {};
 
 export const productActions = {
-  getProductList,
-  createProduct,
-  deleteProduct,
-  editProduct,
-  getProductDetail,
+    getProductList,
+    createProduct,
+    deleteProduct,
+    editProduct,
+    getProductDetail,
 };
--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -2,8 +2,18 @@ import * as types from "../constants/product.constants";
 const initialState = {};
 
 function productReducer(state = initialState, action) {
-  const { type, payload } = action;
-  return state;
+    const { type, payload } = action;
+    switch (type) {
+        case types.PRODUCT_CREATE_REQUEST:
+            return { ...state, loading: true };
+        case types.PRODUCT_CREATE_SUCCESS:
+            return { ...state, loading: false };
+        case types.PRODUCT_CREATE_FAIL:
+            return { ...state, loading: false, error: payload };
+
+        default:
+            return state;
+    }
 }
 
 export default productReducer;
