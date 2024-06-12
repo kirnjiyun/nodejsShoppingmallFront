c9da7b6c4f6d61e9a6a16555c8f1a33917c4fdfa
kirnjiyun <kimjiyunee@naver.com>
2024-06-12 13:09:40 +0900

    내 주문 페이지

--- src/component/OrderStatusCard.js
+++ src/component/OrderStatusCard.js
@@ -3,34 +3,50 @@ import { Row, Col, Badge } from "react-bootstrap";
 import { badgeBg } from "../constants/order.constants";
 import { currencyFormat } from "../utils/number";
 
-const OrderStatusCard = () => {
-  return (
-    <div>
-      <Row className="status-card">
-        <Col xs={2}>
-          <img
-            src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fb3%2F10%2Fb310d46e8f33571ea44cc4facf3cd224a90ef3d4.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
-            alt=""
-            height={96}
-          />
-        </Col>
-        <Col xs={8} className="order-info">
-          <div>
-            <strong>주문번호: "hard_code"</strong>
-          </div>
+const OrderStatusCard = ({ orderItem }) => {
+    const totalPrice = orderItem.items.reduce((total, item) => {
+        const price = item.price || 0; // item 객체에서 price 가져오기
+        console.log("Item:", item);
+        console.log("Product ID:", item.productId);
+        console.log("Price:", price, "Quantity:", item.qty);
+        return total + price * item.qty;
+    }, 0);
 
-          <div className="text-12">2023-03-31</div>
+    return (
+        <div>
+            <Row className="status-card">
+                <Col xs={2}>
+                    <img
+                        src={orderItem.items[0]?.productId?.image}
+                        alt={orderItem.items[0]?.productId?.image}
+                        height={96}
+                    />
+                </Col>
+                <Col xs={8} className="order-info">
+                    <div>
+                        <strong>주문번호: {orderItem.orderNum}</strong>
+                    </div>
 
-          <div>리넨셔츠 외 1개</div>
-          <div>₩ 45,000</div>
-        </Col>
-        <Col md={2} className="vertical-middle">
-          <div className="text-align-center text-12">주문상태</div>
-          <Badge bg="warning">preparing</Badge>
-        </Col>
-      </Row>
-    </div>
-  );
+                    <div className="text-12">
+                        {orderItem.createdAt.slice(0, 10)}
+                    </div>
+
+                    <div>
+                        {orderItem.items[0].productId?.name}
+                        {orderItem.items.length > 1 &&
+                            ` 외 ${orderItem.items.length - 1}개`}
+                    </div>
+                    <div>₩ {currencyFormat(totalPrice)}</div>
+                </Col>
+                <Col md={2} className="vertical-middle">
+                    <div className="text-align-center text-12">주문상태</div>
+                    <Badge bg={badgeBg[orderItem.status]}>
+                        {orderItem.status}
+                    </Badge>
+                </Col>
+            </Row>
+        </div>
+    );
 };
 
 export default OrderStatusCard;
--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -7,16 +7,32 @@ import OrderStatusCard from "../component/OrderStatusCard";
 import "../style/orderStatus.style.css";
 
 const MyPage = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  //오더리스트 들고오기
-
-  // 오더리스트가 없다면? 주문한 상품이 없습니다 메세지 보여주기
-  return (
-    <Container className="status-card-container">
-      <OrderStatusCard />
-    </Container>
-  );
+    //오더리스트 들고오기
+    const { orderList } = useSelector((state) => state.order);
+    useEffect(() => {
+        dispatch(orderActions.getOrder());
+    }, []);
+    // 오더리스트가 없다면? 주문한 상품이 없습니다 메세지 보여주기
+    if (orderList?.length === 0) {
+        return (
+            <Container className="no-order-box">
+                <div>진행중인 주문이 없습니다.</div>
+            </Container>
+        );
+    }
+    return (
+        <Container className="status-card-container">
+            {orderList.map((item) => (
+                <OrderStatusCard
+                    orderItem={item}
+                    className="status-card-container"
+                    key={item._id}
+                />
+            ))}
+        </Container>
+    );
 };
 
 export default MyPage;
