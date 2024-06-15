b0161493d9a8dc0485c35e60265fa5f94e55da44
kirnjiyun <kimjiyunee@naver.com>
2024-06-16 01:17:37 +0900

    redirect_uri

--- src/component/KakaoLogin.jsx
+++ src/component/KakaoLogin.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const KakaoLogin = ({ onSuccess }) => {
     const KAKAO_REST_KEY = process.env.REACT_APP_KAKAO_REST_KEY;
-    const redirect_uri = process.env.REACT_APP_PROD_REDIRECT_URI;
+    const redirect_uri =
+        process.env.REACT_APP_REDIRECT_URI ||
+        "https://www.jiyun-shopping.netlify.app/auth";
+
     console.log("Redirect URI: ", redirect_uri);
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_KEY}&redirect_uri=${redirect_uri}&response_type=code`;
 
